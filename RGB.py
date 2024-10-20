80e3d8461d27484ccc82e2d325e5e2efc4652eb2
sanjaysan12 <ksanjay02444@gmail.com>
2024-10-20 11:29:56 +0530

    Updated RGB

---
+++ RGB.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+channel = [13,19,12]
+for c in channel:
+	GPIO.setup(c,GPIO.OUT)
+try:
+	while True:
+		try:
+			i = input("Enter a number between 0-7: ")
+			i = int(i)
+			if (i < 0 or i >= 8):
+				print("Invalid range of input.... Try Again...")
+				continue
+			rgb = format(i,'03b')
+			for i,c in enumerate(channel):
+				print(i,c,rgb[i])
+				GPIO.output(c,not bool(int(rgb[i])))
+		except ValueError:
+			print("Invalid input try again")
+
+except KeyboardInterrupt:
+	GPIO.cleanup()
+	print("Quitting")
